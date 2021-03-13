af66fd9b131b6f27519668fdc82da8ed1afc1d3b
Ilhomidin Bahoraliev <itilhomidin@yandex.com>
2021-03-13 09:31:21 +0500

    Add asshole operation

--- fast_foaas.py
+++ fast_foaas.py
@@ -5,5 +5,9 @@ def anyway(company: str, name: str) -> str:
     return f"Who the fuck are you anyway, {company}, why are you stirring up so much trouble, and, who pays you? - {name}"
 
 
-__all__ = ["anyway"]
+def ashole(name: str) -> str:
+    return f"Fuck you, asshole. - {name}"
+
+
+__all__ = ["anyway", "ashole"]
 __version__ = "2.2.0"
--- fast_foass_test.py
+++ fast_foass_test.py
@@ -6,3 +6,7 @@ def test_anyway():
         fast_foaas.anyway("LM", "Sam")
         == "Who the fuck are you anyway, LM, why are you stirring up so much trouble, and, who pays you? - Sam"
     )
+
+
+def test_ashole():
+    assert fast_foaas.ashole("Sam") == "Fuck you, asshole. - Sam"
