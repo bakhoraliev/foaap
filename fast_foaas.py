ddcdac2ffd30a5e1a3c5d380d9226fc73622b6a6
Ilhomidin Bahoraliev <itilhomidin@yandex.com>
2021-03-13 09:29:46 +0500

    Add anyway operation

---
+++ fast_foaas.py
@@ -0,0 +1,9 @@
+"""Faster version of FOAAS"""
+
+
+def anyway(company: str, name: str) -> str:
+    return f"Who the fuck are you anyway, {company}, why are you stirring up so much trouble, and, who pays you? - {name}"
+
+
+__all__ = ["anyway"]
+__version__ = "2.2.0"
---
+++ fast_foass_test.py
@@ -0,0 +1,8 @@
+import fast_foaas
+
+
+def test_anyway():
+    assert (
+        fast_foaas.anyway("LM", "Sam")
+        == "Who the fuck are you anyway, LM, why are you stirring up so much trouble, and, who pays you? - Sam"
+    )
